1199556dd929ae0c60871fd1225725109d285938
Lenovo <Lenovo@DESKTOP-T0G3DCC>
2017-09-23 01:09:15 +0300

    Initial Commit

---
+++ src/main/java/demo/rest/secure/OrderReceipt/OrderReceiptApplication.java
@@ -0,0 +1,12 @@
+package demo.rest.secure.OrderReceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderReceiptApplication.class, args);
+	}
+}
---
+++ src/test/java/demo/rest/secure/OrderReceipt/OrderReceiptApplicationTests.java
@@ -0,0 +1,16 @@
+package demo.rest.secure.OrderReceipt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderReceiptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
